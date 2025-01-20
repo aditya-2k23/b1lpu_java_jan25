fef842091299948471f80c69897241116f3c8b83
Mohit Bisht <mohitbisht@192.168.1.23>
2025-01-20 20:51:44 +0530

    Day2

---
+++ Day2/Basics.java
@@ -0,0 +1,13 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Basics {
+       public static void main(String[] args) {
+              System.out.println("hi just a demo to show how it works!");
+              Scanner sc = new Scanner(System.in);
+              int a = sc.nextInt();
+              System.out.println("number is "+a);
+              sc.close();
+       }
+}
