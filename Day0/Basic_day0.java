877115ce08f61b9cdbb120e20a250e8275fbb4d0
Mohit <59963561+Mohitbisht3031@users.noreply.github.com>
2025-01-20 19:58:12 +0530

    Add files via upload
    Day0

---
+++ Day0/Basic_day0.java
@@ -0,0 +1,7 @@
+
+public class Basic_day0 {
+    public static void main(String args[]) {
+        System.out.println("welcome to the core java class! " +System.currentTimeMillis());
+        System.out.print("Mohit");
+    }
+}
