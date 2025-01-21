01f1f8836c1c478aef184a453609e312c0d2c385
aditya-2k23 <holaaditya123@gmail.com>
2025-01-21 22:49:23 +0530

    format code in Basics.java for consistency

--- Day2/Basics.java
+++ Day2/Basics.java
@@ -3,11 +3,11 @@ package Day2;
 import java.util.Scanner;
 
 public class Basics {
-       public static void main(String[] args) {
-              System.out.println("hi just a demo to show how it works!");
-              Scanner sc = new Scanner(System.in);
-              int a = sc.nextInt();
-              System.out.println("number is "+a);
-              sc.close();
-       }
+    public static void main(String[] args) {
+        System.out.println("hi just a demo to show how it works!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("number is " + a);
+        sc.close();
+    }
 }
