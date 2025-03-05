e9eea79c706f45801a9b4c21ffac7909f70cfbbe
Mohit Bisht <mohitbisht@192.168.1.31>
2025-03-05 19:55:23 +0530

    Day18

---
+++ Day18/Inherit.java
@@ -0,0 +1,41 @@
+package Day18;
+
+// Multilevel
+class  Animal {
+    
+}
+class dog extends Animal{
+
+}
+class puppy extends dog{
+
+}
+
+// Heri
+class Animal{
+
+}
+class DOG extends Animal{
+
+}
+class Cat extends Animal{
+
+}
+
+// Hybrid
+class Animal{
+
+}
+class Dog extends Animal{
+
+}
+class Puppy extends Dog{
+
+}
+class CAT extends Animal{
+
+}
+
+public class Inherit {
+    
+}
